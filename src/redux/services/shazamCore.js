7f3034d5867f45ec6ab585c1621f99cef0ab5927
Vikrant Shitole <vs232@enovate-it.com>
2023-12-02 23:13:08 +0530

    Artists Details page done

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,7 +14,7 @@ const options = {
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key','22e3ee9ae2msh01377fa93cebad4p1ca4e7jsnc63c5f8d44be')
             return headers
@@ -22,11 +22,12 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({
-            query: () => '/charts/world'
+            query: () => 'v1/charts/world'
         }),
-        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-     
+        getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+
     })
 })
 
@@ -34,5 +35,6 @@ export const {
     useGetTopChartsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
-  
+    useGetArtistDetailsQuery,
+
 } = shazamCoreApi
